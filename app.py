a90333e383f353465ab592f391a8b2b88b614869
Denis Ruiz <dlruiz7@utpl.edu.ec>
2025-04-16 11:38:14 -0500

    subida

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ def home():
     url = "https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt"
     response = requests.get(url)
 
-    if response.status_code != 200:
-        return "<p>Error al obtener el archivo desde la URL.</p>"
-
     lines = response.text.strip().split("\n")
     people = []
 
@@ -30,7 +27,6 @@ def home():
             })
 
     html = '''
-    <h1>Personas con ID que comienza en 3, 4, 5 o 7</h1>
     <table border="1" cellpadding="5">
         <tr>
             <th>ID</th>
